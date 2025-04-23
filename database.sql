09eceed1db4cb50aff0cff003dc27734648b9816
Youssef Amr <youssea70@gmail.com>
2025-04-23 16:22:48 +0200

    add a default time

--- database.sql
+++ database.sql
@@ -17,7 +17,7 @@ CREATE TABLE assets (
     type VARCHAR(255) NOT NULL,
     status VARCHAR(50), -- the status of asset (in use, available, under maintenance, ...etc)
     description TEXT, 
-    add_date DATETIME, -- date and time the asset was added
+    add_date DATETIME DEFAULT NOW(), -- date and time the asset was added if you don't say it will add the current
     user_id INT, -- if the asset in use add user_id to know who use it
     FOREIGN KEY (user_id) REFERENCES users(u_id) ON DELETE SET NULL
 );
@@ -26,7 +26,7 @@ CREATE TABLE assets (
 CREATE TABLE asset_history (
     user_id INT,
     asset_id INT NOT NULL,
-    date DATETIME,
+    date DATETIME DEFAULT NOW(),
     status VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(u_id) ON DELETE CASCADE,
     FOREIGN KEY (asset_id) REFERENCES assets(as_id) ON DELETE CASCADE
